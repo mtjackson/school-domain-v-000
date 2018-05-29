006db8cafb8594a901ead8af0092dc9ccc42e3d9
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-29 21:48:53 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,11 +10,11 @@ class School
   end
 
   def add_student(student, grade)
-    if roster.has_key?(grade)
-      roster[grade] << student
+    if @roster.has_key?(grade)
+      @roster[grade] << student
     else
-      roster[grade] = []
-      roster[grade] << student
+      @roster[grade] = []
+      @roster[grade] << student
     end
   end
 end
