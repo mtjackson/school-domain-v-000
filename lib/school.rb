9d57902a8b932ca5d5787fc03ce8f29b090eeb23
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-29 21:40:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  def initialize(school_name)
+    @school_name = school_name
+  end
+
+  def roster
+    @roster = Hash.new
+  end
+
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+end
